0e59ed610879ac8152d3bbbee48e9281e47592b0
Johannes Tenhumberg <johannes.tenhumberg@gmail.com>
2024-11-04 13:23:11 +0100

    check switch2cal on mac

--- wzk/cpp2py/MinSphere/setup.py
+++ wzk/cpp2py/MinSphere/setup.py
@@ -7,10 +7,17 @@ HOMEBREW_PREFIX = "/opt/homebrew/Cellar"
 BOOST_DIR = f"{HOMEBREW_PREFIX}/boost"
 CGAL_DIR = f"{HOMEBREW_PREFIX}/cgal"
 
-boost_include_dir = os.environ.get("BOOST_INCLUDE_DIR",  # either user-defined
-                                   f"{BOOST_DIR}/{files.listdir(BOOST_DIR)[0]}/include")  # or installed via homebrew
-cgal_include_dir = os.environ.get("CGAL_INCLUDE_DIR",  # either user-defined
-                                  f"{CGAL_DIR}/{files.listdir(CGAL_DIR)[0]}/include")  # or installed via homebrew
+
+CONDA_PREFIX = os.environ.get("CONDA_PREFIX")
+
+boost_include_dir = f"{CONDA_PREFIX}/include/boost"
+cgal_include_dir = f"{CONDA_PREFIX}/include/CGAL22"  # why does this work??
+
+# TODO add a switch here
+# boost_include_dir = os.environ.get("BOOST_INCLUDE_DIR",  # either user-defined
+#                                   f"{BOOST_DIR}/{files.listdir(BOOST_DIR)[0]}/include")  # or installed via homebrew
+# cgal_include_dir = os.environ.get("CGAL_INCLUDE_DIR",  # either user-defined
+#                                  f"{CGAL_DIR}/{files.listdir(CGAL_DIR)[0]}/include")  # or installed via homebrew
 
 ext = Extension(
     name="wzkMinSphere",
